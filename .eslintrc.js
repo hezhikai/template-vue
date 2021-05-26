48f42f9b8a80ebcb8348b48a5fee99f611f90961
hezhikai <hezhikai@huafeng.com>
2021-05-26 14:25:41 +0800

    feat(依赖改造): 使用npm的工具包代替现有配置
    不向下兼容，测试代码，未能正常使用
    
    BREAKING CHANGE: 引用的依赖未上传至npm，只在本地link使用

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,3 @@
-const prettierrc = require('./.prettierrc.js');
-
 module.exports = {
-  root: true,
-  // parser: 'babel-eslint',
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 6,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  env: {
-    node: true,
-    browser: true,
-    es6: true,
-    amd: true,
-    jest: true // 支持 jest 语法
-  },
-  globals: {},
-  extends: ['plugin:vue/essential', 'eslint:recommended', 'prettier'],
-  plugins: ['html', 'import', 'json', 'node', 'promise', 'vue', 'prettier'],
-  rules: {
-    'prettier/prettier': ['error', prettierrc], // 不符合 prettier 规则的代码，要进行错误提示（红线）
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  extends: ['eslint-config-hzkweb']
 };
